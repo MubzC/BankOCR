e6cf4f31d1dff12a6d40ece064a7d926d8d6c6bb
MubzC <mubinchachia@gmail.com>
2021-09-24 20:13:30 +0100

    Initial Skeleton Code Commitq
    Simple Console app commit before work can be done

---
+++ Bank OCR Reader/Bank OCR Reader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bank_OCR_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
