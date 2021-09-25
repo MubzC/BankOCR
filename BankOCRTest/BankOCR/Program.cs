adba53f596e90c1400073588b6fd76e00380f808
MubzC <mubinchachia@gmail.com>
2021-09-25 14:47:57 +0100

    Fixed spacing error in test case. Also user story one done

--- BankOCRTest/BankOCR/Program.cs
+++ BankOCRTest/BankOCR/Program.cs
@@ -1,14 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BankOCR
 {
     // https://codingdojo.org/kata/BankOCR/
     
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static readonly Dictionary<string, string> numberCodes = new Dictionary<string, string>
         {
-            Console.WriteLine("Hello");
+            { " _ | ||_|", "0" },
+            { "     |  |", "1" },
+            { " _  _||_ ", "2" },
+            { " _  _| _|", "3" },
+            { "   |_|  |", "4" },
+            { " _ |_  _|", "5" },
+            { " _ |_ |_|", "6" },
+            { " _   |  |", "7" },
+            { " _ |_||_|", "8" },
+            { " _ |_| _|", "9" },
+        };
+
+        public static void Main(string[] args)
+        {
+            string accountString = @"
+                           
+  |  |  |  |  |  |  |  |  |
+  |  |  |  |  |  |  |  |  |";
+
+            string[] lines = accountString.Split(Environment.NewLine);
+            string accountNumber = "";
+            for (int i = 0; i < 9; i++)
+            {
+                string charToDecipher = "";
+                charToDecipher += lines[1].Substring((i * 3), 3);
+                charToDecipher += lines[2].Substring((i * 3), 3);
+                charToDecipher += lines[3].Substring((i * 3), 3);
+                accountNumber += numberCodes[charToDecipher];
+            }
+            Console.WriteLine(accountNumber);
         }
     }
 }
